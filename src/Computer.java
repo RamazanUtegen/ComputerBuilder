1faab0086c356368b06b0af574580f0c770c5506
ramaz <ramazanutegen583@gmail.com>
2026-09-08 12:01:53 +0500

    Create Computer product class with core attributes

--- src/Computer.java
+++ src/Computer.java
@@ -3,15 +3,20 @@ public class Computer {
     private String gpu;
     private String ram;
     private String storage;
+    private String formFactor;
+    private boolean hasGlassPanel;
 
     public void setCpu(String cpu) { this.cpu = cpu; }
     public void setGpu(String gpu) { this.gpu = gpu; }
     public void setRam(String ram) { this.ram = ram; }
     public void setStorage(String storage) { this.storage = storage; }
+    public void setFormFactor(String formFactor) { this.formFactor = formFactor; }
+    public void setGlassPanel(boolean hasGlassPanel) { this.hasGlassPanel = hasGlassPanel; }
 
     @Override
     public String toString() {
-        return "Computer Spec: CPU=" + cpu + ", GPU=" + gpu +
-                ", RAM=" + ram + ", Storage=" + storage;
+        return "Computer [" + formFactor + "] -> CPU: " + cpu + ", GPU: " + gpu +
+                ", RAM: " + ram + ", Storage: " + storage +
+                ", Glass Panel: " + (hasGlassPanel ? "Yes" : "No");
     }
 }
